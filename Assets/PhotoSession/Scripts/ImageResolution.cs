c064fcfc35f3400bb34b8dac7d00b11818b1d327
Roland09 <roland.cidar@gmail.com>
2021-03-19 14:39:42 +0100

    added 8K preset

--- Assets/PhotoSession/Scripts/ImageResolution.cs
+++ Assets/PhotoSession/Scripts/ImageResolution.cs
@@ -9,12 +9,11 @@ namespace Rowlan.PhotoSession
 	{
 		public static ImageResolution PresetGame = new ImageResolution(ImageResolutionType.Game, -1, -1);
 		public static ImageResolution PresetFullHd = new ImageResolution(ImageResolutionType.FullHd, 1920, 1080);
-		public static ImageResolution PresetUhd = new ImageResolution(ImageResolutionType.Uhd, 3840, 2160);
-		public static ImageResolution PresetUltraWide = new ImageResolution(ImageResolutionType.UltraWide, 3840, 1080);
-		public static ImageResolution PresetFourK = new ImageResolution(ImageResolutionType.FourK, 4096, 2160);
+		public static ImageResolution PresetFourK = new ImageResolution(ImageResolutionType.FourK, 3840, 2160);
+		public static ImageResolution PresetEightK = new ImageResolution(ImageResolutionType.EightK, 7680, 4320);
 
 		public static ImageResolution[] imageResolutions = new ImageResolution[] {
-			PresetGame, PresetFullHd, PresetUhd, PresetUltraWide, PresetFourK
+			PresetGame, PresetFullHd, PresetFourK, PresetEightK
 		};
 
 		public enum ImageResolutionType
@@ -25,14 +24,11 @@ namespace Rowlan.PhotoSession
 			[InspectorName("Full HD (1920 x 1080, 16:9)")]
 			FullHd,
 
-			[InspectorName("Ultra HD (3840 x 2160, 16:9)")]
-			Uhd,
-
-			[InspectorName("Ultra Wide (3840 x 1080, 32:9)")]
-			UltraWide,
-
-			[InspectorName("4K (4096 x 2160, 19:10)")]
+			[InspectorName("4K (3840 x 2160, 19:9)")]
 			FourK,
+
+			[InspectorName("8K (7680 x 4320, 16:9)")]
+			EightK,
 
 		}
 
@@ -57,6 +53,9 @@ namespace Rowlan.PhotoSession
 
 			if (imageResolution != null)
 				return imageResolution;
+
+
+			Debug.LogError("Resolution not found: " + imageResolution);
 
 			return ImageResolution.PresetGame;
 		}
