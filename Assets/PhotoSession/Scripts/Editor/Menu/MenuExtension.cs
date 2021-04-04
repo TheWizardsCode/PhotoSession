7e20e55e473f997071fd8dcca98050aecf25901b
Roland09 <roland.cidar@gmail.com>
2021-04-04 07:58:24 +0200

    added menu to create prefab

---
+++ Assets/PhotoSession/Scripts/Editor/Menu/MenuExtension.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+
+
+namespace Rowlan.PhotoSession
+{
+    [ExecuteInEditMode]
+    public class MenuExtension : MonoBehaviour
+    {
+
+        [MenuItem("GameObject/Photo Session/Add Prefab", false, 10)]
+        static void AddPrefab(MenuCommand menuCommand)
+        {
+            // create new gameobject
+            GameObject go = Instantiate( Resources.Load("Prefabs/Photo Session")) as GameObject;
+
+            // settings
+            go.name = "Photo Session";
+
+            // ensure gameobject gets reparented if this was a context click (otherwise does nothing)
+            GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+
+            // register the creation in the undo system
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+
+            Selection.activeObject = go;
+
+        }
+    }
+}
