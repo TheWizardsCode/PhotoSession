924ec3f9bdf8b7b43c8ed1dc3787a85bf661eaad
Ross Gardler <ross@gardler.org>
2021-10-30 12:19:59 -0700

    missed adding the moved file

---
+++ Assets/PhotoSession/Scripts/Runtime/AutoSetup.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rowlan.PhotoSession
+{
+    public class AutoSetup
+    {
+        private static readonly string tag_MainCamera = "MainCamera";
+
+        private static readonly string[] script_Blacklist = {
+            "SimpleCameraController", // Unity HDRP Demo
+            "LookWithMouse", // Unity HDRP Demo
+            "CharacterController", // Unity HDRP Demo
+            "PlayerMovement", // Unity HDRP Demo
+            "MFreeLookCamera", // Malbers FreeLookCameraRig
+            "CameraWallStop", // Malbers FreeLookCameraRig
+            "CinemachineBrain"
+            };
+
+        public static void Execute(PhotoSession photoSession)
+        {
+
+            AssignMainCamera( photoSession);
+
+            AssignScriptBlacklist( photoSession);
+        }
+
+        /// <summary>
+        /// Attempt to autoconfigure the camera, if it is not already set.
+        /// Log a warning about needing to set the camera if successfull,
+        /// log an error if not successful.
+        /// </summary>
+        private static void AssignMainCamera(PhotoSession photoSession)
+        {
+
+            // find all cameras
+            Camera[] cameras = Object.FindObjectsOfType<Camera>();
+
+            Camera mainCamera = null;
+            foreach (Camera camera in cameras)
+            {
+
+                // skip all inaktive
+                if (!camera.isActiveAndEnabled)
+                    continue;
+
+                // last one wins unless it's tagged as maincamera, then that one wins
+                if (!mainCamera || camera.CompareTag(tag_MainCamera))
+                {
+                    mainCamera = camera;
+                }
+
+            }
+
+            if (mainCamera)
+            {
+                photoSession.settings.photoCamera = mainCamera;
+            }
+
+            if (!photoSession.settings.photoCamera)
+            {
+                Debug.LogError("No camera found in the scene.");
+            }
+        }
+
+        private static void AssignScriptBlacklist(PhotoSession photoSession)
+        {
+
+            Camera photoCamera = photoSession.settings.photoCamera as Camera;
+
+            if (!photoCamera)
+                return;
+
+            List<Component> components = new List<Component>();
+
+            foreach (string scriptName in script_Blacklist)
+            {
+
+                // recursively search the blacklisted objects from the photo camera gameobject up to the root
+                Transform transform = photoCamera.transform;
+                while (transform)
+                {
+
+                    Component component = transform.gameObject.GetComponent(scriptName);
+
+                    if (component)
+                        components.Add(component);
+
+                    transform = transform.parent;
+                }
+            }
+
+            // set the disabled components in the serializedObject
+            photoSession.settings.disabledComponents = components.ToArray();
+		}
+    }
+}
